c7cf52f521e0238336579e03f3fe66a020abac76
radepal <radoslaw.palczynski@nordcloud.com>
2019-07-30 14:43:51 +0200

    Terraform12 (#20)
    * Init terraform 0.12
    
    * fix concating list of subnets
    
    * adding ? 1 : 0 to counts
    
    * Fix for workers subnets list
    
    * Fix enable_helm counts
    
    * Fix for map_roles varialbe type to list(map(string))
    
    * Update documentation
    
    * cleanup locals
    
    * fix for metric server

--- locals.tf
+++ locals.tf
@@ -1,17 +1,17 @@
 locals {
   worker_group = [
     {
-      name                  = "node"                                                  # Name of the worker group. Literal count.index will never be used but if name is not set, the count.index interpolation will be used.
-      asg_desired_capacity  = "${var.desired_worker_nodes}"                           # Desired worker capacity in the autoscaling group.
-      asg_max_size          = "${var.max_worker_nodes}"                               # Maximum worker capacity in the autoscaling group.
-      asg_min_size          = "${var.min_worker_nodes}"                               # Minimum worker capacity in the autoscaling group.
-      instance_type         = "${var.worker_node_instance_type}"                      # Size of the workers instances.
-      key_name              = "${var.key_name}"                                       # The key name that should be used for the instances in the autoscaling group
-      pre_userdata          = "${data.template_file.http_proxy_workergroup.rendered}" # userdata to pre-append to the default userdata.
-      additional_userdata   = ""                                                      # userdata to append to the default userdata.
-      subnets               = "${join(",", var.private_subnets)}"                     # A comma delimited string of subnets to place the worker nodes in. i.e. subnet-123,subnet-456,subnet-789
-      autoscaling_enabled   = "${var.enable_cluster_autoscaling}"
-      protect_from_scale_in = "${var.protect_cluster_from_scale_in}"
+      name                  = "node"                                             # Name of the worker group. Literal count.index will never be used but if name is not set, the count.index interpolation will be used.
+      asg_desired_capacity  = var.desired_worker_nodes                           # Desired worker capacity in the autoscaling group.
+      asg_max_size          = var.max_worker_nodes                               # Maximum worker capacity in the autoscaling group.
+      asg_min_size          = var.min_worker_nodes                               # Minimum worker capacity in the autoscaling group.
+      instance_type         = var.worker_node_instance_type                      # Size of the workers instances.
+      key_name              = var.key_name                                       # The key name that should be used for the instances in the autoscaling group
+      pre_userdata          = data.template_file.http_proxy_workergroup.rendered # userdata to pre-append to the default userdata.
+      additional_userdata   = ""                                                 # userdata to append to the default userdata.
+      subnets               = var.private_subnets                                # A list of string of subnets to place the worker nodes in. i.e. subnet-123,subnet-456,subnet-789
+      autoscaling_enabled   = var.enable_cluster_autoscaling
+      protect_from_scale_in = var.protect_cluster_from_scale_in
     },
   ]
 
@@ -19,14 +19,14 @@ locals {
 
   cluster_autoscaler_defaults = {
     namespace               = "kube-system"
-    scale-down-enabled      = "${var.protect_cluster_from_scale_in}"
+    scale-down-enabled      = var.protect_cluster_from_scale_in
     scale-down-uneeded-time = 10
     scan-interval           = 10
   }
 
-  enable_helm = "${var.enable_cluster_autoscaling || var.enable_pod_autoscaling || var.install_helm ? 1 : 0}"
+  enable_helm = var.enable_cluster_autoscaling || var.enable_pod_autoscaling || var.install_helm ? 1 : 0
 
-  enable_cluster_autoscaling = "${var.enable_cluster_autoscaling}"
+  enable_cluster_autoscaling = var.enable_cluster_autoscaling
 
   master_config_services_proxy = [
     {
@@ -34,7 +34,7 @@ locals {
       type = "daemonset"
     },
     {
-      name = "coredns"    # In Kubernetes v1.10: dns is called 'kube-dns'; in v1.11+, dns is called 'coredns', but still has the app tag 'kube-dns'
+      name = "coredns" # In Kubernetes v1.10: dns is called 'kube-dns'; in v1.11+, dns is called 'coredns', but still has the app tag 'kube-dns'
       type = "deployment"
     },
     {
@@ -45,21 +45,26 @@ locals {
 
   no_proxy_default = "localhost,127.0.0.1,169.254.169.254,10.0.0.0/8,172.16.0.0/12,192.168.0.0/16,.local,.internal,.elb.amazonaws.com,.elb.${var.region}.amazonaws.com"
 
-  no_proxy_merged = "${join(",", distinct(concat(split(",", local.no_proxy_default), split(",", var.no_proxy))))}"
+  no_proxy_merged = join(
+    ",",
+    distinct(
+      concat(split(",", local.no_proxy_default), split(",", var.no_proxy)),
+    ),
+  )
 
   maps_roles_default = [
     {
-      role_arn = "${var.enable_default_roles ? aws_iam_role.cluster_admin.arn : ""}"
+      role_arn = var.enable_default_roles ? aws_iam_role.cluster_admin[0].arn : ""
       username = "admin"
       group    = "system:masters"
     },
     {
-      role_arn = "${var.enable_default_roles ? aws_iam_role.cluster_view.arn : ""}"
+      role_arn = var.enable_default_roles ? aws_iam_role.cluster_view[0].arn : ""
       username = "view"
       group    = "view"
     },
   ]
 
-  map_roles       = "${concat(local.maps_roles_default, var.map_roles)}"
-  map_roles_count = "${var.enable_default_roles ? length(var.map_roles) + 2 : length(var.map_roles)}" # +2 is beacue terraform 0.11 issue with count
+  map_roles = concat(local.maps_roles_default, var.map_roles)
 }
+
--- main.tf
+++ main.tf
@@ -1,80 +1,79 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "2.3.1"
+  version = "5.0.0"
 
-  cluster_name                               = "${var.cluster_prefix}"
-  subnets                                    = ["${compact(concat(var.private_subnets, var.public_subnets))}"]
+  cluster_name                               = var.cluster_prefix
+  subnets                                    = concat(var.private_subnets, var.public_subnets)
   write_kubeconfig                           = true
-  config_output_path                         = "${var.outputs_directory}"
-  tags                                       = "${var.tags}"
-  vpc_id                                     = "${var.vpc_id}"
-  worker_groups                              = "${local.worker_group}"
-  kubeconfig_aws_authenticator_env_variables = "${var.aws_authenticator_env_variables}"
-  worker_group_count                         = "1"
-  worker_additional_security_group_ids       = ["${aws_security_group.all_worker_additional.id}"]
-  cluster_version                            = "${var.cluster_version}"
+  config_output_path                         = var.outputs_directory
+  tags                                       = var.tags
+  vpc_id                                     = var.vpc_id
+  worker_groups                              = local.worker_group
+  kubeconfig_aws_authenticator_env_variables = var.aws_authenticator_env_variables
+  worker_additional_security_group_ids       = [aws_security_group.all_worker_additional.id]
+  cluster_version                            = var.cluster_version
 
-  map_roles          = "${local.map_roles}"
-  map_roles_count    = "${local.map_roles_count}"
-  map_users          = "${var.map_users}"
-  map_users_count    = "${var.map_users_count}"
-  map_accounts       = "${var.map_accounts}"
-  map_accounts_count = "${var.map_accounts_count}"
+  map_roles    = local.map_roles
+  map_users    = var.map_users
+  map_accounts = var.map_accounts
+
 }
 
 resource "aws_security_group" "all_worker_additional" {
   name_prefix = "all_worker_additional"
-  vpc_id      = "${var.vpc_id}"
+  vpc_id      = var.vpc_id
 }
 
 resource "aws_security_group_rule" "allow_ssh" {
-  count       = "${length(var.allowed_worker_ssh_cidrs) != 0 ? 1 : 0}"
+  count       = length(var.allowed_worker_ssh_cidrs) != 0 ? 1 : 0
   type        = "ingress"
   from_port   = 22
   to_port     = 22
   protocol    = "tcp"
-  cidr_blocks = ["${var.allowed_worker_ssh_cidrs}"]
+  cidr_blocks = var.allowed_worker_ssh_cidrs
   description = "Allow SSH connections"
 
-  security_group_id = "${aws_security_group.all_worker_additional.id}"
+  security_group_id = aws_security_group.all_worker_additional.id
 }
 
 resource "aws_security_group_rule" "allow_ingress" {
-  count       = "${var.ingress_deploy}"
+  count       = var.ingress_deploy ? 1 : 0
   type        = "ingress"
-  from_port   = "${var.ingress_service_nodeport_http}"
-  to_port     = "${var.ingress_service_nodeport_http}"
+  from_port   = var.ingress_service_nodeport_http
+  to_port     = var.ingress_service_nodeport_http
   protocol    = "tcp"
-  cidr_blocks = ["${var.allowed_worker_nodeport_cidrs}"]
+  cidr_blocks = var.allowed_worker_nodeport_cidrs
   description = "Allow connections to ingress exposed via NodePort"
 
-  security_group_id = "${aws_security_group.all_worker_additional.id}"
+  security_group_id = aws_security_group.all_worker_additional.id
 }
 
 data "template_file" "http_proxy_workergroup" {
-  template = "${file("${path.module}/templates/workergroup_proxy.tpl")}"
+  template = file("${path.module}/templates/workergroup_proxy.tpl")
 
-  vars {
-    http_proxy   = "${var.http_proxy}"
-    https_proxy  = "${var.http_proxy}"
-    no_proxy     = "${local.no_proxy_merged}"
-    cluster_name = "${var.cluster_prefix}"
+  vars = {
+    http_proxy   = var.http_proxy
+    https_proxy  = var.http_proxy
+    no_proxy     = local.no_proxy_merged
+    cluster_name = var.cluster_prefix
   }
 }
 
 data "template_file" "proxy_environment_variables" {
-  template = "${file("${path.module}/templates/proxy-environment-variables.yaml.tpl")}"
+  template = file(
+    "${path.module}/templates/proxy-environment-variables.yaml.tpl",
+  )
 
-  vars {
-    http_proxy  = "${var.http_proxy}"
-    https_proxy = "${var.http_proxy}"
-    no_proxy    = "${local.no_proxy_merged}"
+  vars = {
+    http_proxy  = var.http_proxy
+    https_proxy = var.http_proxy
+    no_proxy    = local.no_proxy_merged
   }
 }
 
 resource "null_resource" "proxy_environment_variables" {
-  count      = "${var.http_proxy != "" ? 1 : 0}"
-  depends_on = ["module.eks"]
+  count      = var.http_proxy != "" ? 1 : 0
+  depends_on = [module.eks]
 
   provisioner "local-exec" {
     command = "echo \"${data.template_file.proxy_environment_variables.rendered}\" | kubectl apply -f - --kubeconfig=\"${var.outputs_directory}kubeconfig_${var.cluster_prefix}\""
@@ -82,11 +81,14 @@ resource "null_resource" "proxy_environment_variables" {
 }
 
 resource "null_resource" "master_config_services_proxy" {
-  count      = "${var.http_proxy != "" ? length(local.master_config_services_proxy) : 0}"
-  depends_on = ["module.eks", "null_resource.proxy_environment_variables"]
+  count = var.http_proxy != "" ? length(local.master_config_services_proxy) : 0
+  depends_on = [
+    module.eks,
+    null_resource.proxy_environment_variables,
+  ]
 
   provisioner "local-exec" {
-    command = "kubectl patch ${lookup(local.master_config_services_proxy[count.index], "type")} ${lookup(local.master_config_services_proxy[count.index], "name")} --namespace kube-system --type='json' -p='[{\"op\": \"add\", \"path\": \"/spec/template/spec/containers/0/envFrom\", \"value\": [{\"configMapRef\": {\"name\": \"proxy-environment-variables\"}}] }]' --kubeconfig=\"${var.outputs_directory}kubeconfig_${var.cluster_prefix}\""
+    command = "kubectl patch ${local.master_config_services_proxy[count.index]["type"]} ${local.master_config_services_proxy[count.index]["name"]} --namespace kube-system --type='json' -p='[{\"op\": \"add\", \"path\": \"/spec/template/spec/containers/0/envFrom\", \"value\": [{\"configMapRef\": {\"name\": \"proxy-environment-variables\"}}] }]' --kubeconfig=\"${var.outputs_directory}kubeconfig_${var.cluster_prefix}\""
   }
 }
 
@@ -95,18 +97,23 @@ resource "null_resource" "validate_dns" {
     command = <<EOC
     /bin/sh \
       "${path.module}/scripts/validate_coredns.sh" "${var.outputs_directory}kubeconfig_${var.cluster_prefix}"
-    EOC
+    
+EOC
+
   }
 
-  depends_on = ["module.eks", "null_resource.master_config_services_proxy"]
+  depends_on = [
+    module.eks,
+    null_resource.master_config_services_proxy,
+  ]
 }
 
 data "template_file" "helm_rbac_config" {
-  template = "${file("${path.module}/templates/helm_rbac_config.yaml.tpl")}"
+  template = file("${path.module}/templates/helm_rbac_config.yaml.tpl")
 }
 
 resource "null_resource" "initialize_helm" {
-  count = "${local.enable_helm}"
+  count = local.enable_helm
 
   provisioner "local-exec" {
     command = "echo \"${data.template_file.helm_rbac_config.rendered}\" | kubectl apply -f - --kubeconfig=\"${var.outputs_directory}kubeconfig_${var.cluster_prefix}\""
@@ -116,43 +123,43 @@ resource "null_resource" "initialize_helm" {
     command = "helm init --service-account tiller --wait --kubeconfig=\"${var.outputs_directory}kubeconfig_${var.cluster_prefix}\""
   }
 
-  depends_on = ["null_resource.validate_dns"]
+  depends_on = [null_resource.validate_dns]
 }
 
 resource "null_resource" "install_metrics_server" {
-  count = "${local.enable_helm}" #only for pod autoscaling
+  count = local.enable_helm #only for pod autoscaling
 
   provisioner "local-exec" {
-    command = "helm install stable/metrics-server --name metrics-server --namespace metrics  --kubeconfig=${var.outputs_directory}kubeconfig_${var.cluster_prefix}"
+    command = "helm install stable/metrics-server --name metrics-server --namespace metrics --set args[0]=--kubelet-insecure-tls,args[1]=--kubelet-preferred-address-types=InternalIP  --kubeconfig=${var.outputs_directory}kubeconfig_${var.cluster_prefix}"
   }
 
-  depends_on = ["null_resource.initialize_helm"]
+  depends_on = [null_resource.initialize_helm]
 }
 
 data "template_file" "cluster_autoscaling" {
-  template = "${file("${path.module}/templates/cluster_autoscaling.yaml.tpl")}"
+  template = file("${path.module}/templates/cluster_autoscaling.yaml.tpl")
 
-  vars {
-    http_proxy   = "${var.http_proxy}"
-    https_proxy  = "${var.http_proxy}"
-    no_proxy     = "${local.no_proxy_merged}"
-    region       = "${var.region}"
-    cluster_name = "${var.cluster_prefix}"
+  vars = {
+    http_proxy = var.http_proxy
+    https_proxy = var.http_proxy
+    no_proxy = local.no_proxy_merged
+    region = var.region
+    cluster_name = var.cluster_prefix
   }
 }
 
 resource "null_resource" "initialize_cluster_autoscaling" {
-  count = "${local.enable_cluster_autoscaling}"
+  count = local.enable_cluster_autoscaling ? 1 : 0
 
   provisioner "local-exec" {
     command = "echo \"${data.template_file.cluster_autoscaling.rendered}\" | helm install -f - stable/cluster-autoscaler --name vertical-scaler --namespace=kube-system --kubeconfig=\"${var.outputs_directory}kubeconfig_${var.cluster_prefix}\""
   }
 
-  depends_on = ["null_resource.initialize_helm"]
+  depends_on = [null_resource.initialize_helm]
 }
 
 resource "null_resource" "install_ingress" {
-  count = "${var.ingress_deploy}"
+  count = var.ingress_deploy ? 1 : 0
 
   provisioner "local-exec" {
     command = <<EOC
@@ -163,46 +170,49 @@ resource "null_resource" "install_ingress" {
         --set controller.service.nodePorts.http="${var.ingress_service_nodeport_http}" \
         --set controller.service.enableHttp=true \
         --set controller.service.enableHttps=false
-    EOC
-  }
+    
+EOC
 
-  depends_on = ["null_resource.initialize_helm"]
+}
+
+depends_on = [null_resource.initialize_helm]
 }
 
 resource "aws_iam_role" "cluster_admin" {
-  count                 = "${var.enable_default_roles}"
-  name                  = "${var.cluster_prefix}-cluster-admin"
-  assume_role_policy    = "${data.aws_iam_policy_document.cluster_assume_role_policy.json}"
-  force_detach_policies = true
+count                 = var.enable_default_roles ? 1 : 0
+name                  = "${var.cluster_prefix}-cluster-admin"
+assume_role_policy    = data.aws_iam_policy_document.cluster_assume_role_policy.json
+force_detach_policies = true
 }
 
 resource "aws_iam_role_policy_attachment" "cluster_admin_AmazonEKSClusterPolicy" {
-  count      = "${var.enable_default_roles}"
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = "${aws_iam_role.cluster_admin.name}"
+count      = var.enable_default_roles ? 1 : 0
+policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+role       = aws_iam_role.cluster_admin[0].name
 }
 
 resource "aws_iam_role_policy_attachment" "cluster_admin_AmazonEKSServicePolicy" {
-  count      = "${var.enable_default_roles}"
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
-  role       = "${aws_iam_role.cluster_admin.name}"
+count      = var.enable_default_roles ? 1 : 0
+policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+role       = aws_iam_role.cluster_admin[0].name
 }
 
 resource "aws_iam_role" "cluster_view" {
-  count                 = "${var.enable_default_roles}"
-  name                  = "${var.cluster_prefix}-cluster-view"
-  assume_role_policy    = "${data.aws_iam_policy_document.cluster_assume_role_policy.json}"
-  force_detach_policies = true
+count                 = var.enable_default_roles ? 1 : 0
+name                  = "${var.cluster_prefix}-cluster-view"
+assume_role_policy    = data.aws_iam_policy_document.cluster_assume_role_policy.json
+force_detach_policies = true
 }
 
 resource "aws_iam_role_policy_attachment" "cluster_view_AmazonEKSClusterPolicy" {
-  count      = "${var.enable_default_roles}"
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = "${aws_iam_role.cluster_view.name}"
+count      = var.enable_default_roles ? 1 : 0
+policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+role       = aws_iam_role.cluster_view[0].name
 }
 
 resource "aws_iam_role_policy_attachment" "cluster_view_AmazonEKSServicePolicy" {
-  count      = "${var.enable_default_roles}"
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
-  role       = "${aws_iam_role.cluster_view.name}"
+count      = var.enable_default_roles ? 1 : 0
+policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+role       = aws_iam_role.cluster_view[0].name
 }
+
