a5583cd5708b19289bdf42a1022f2ee912189098
Radosław Palczyński <radoslaw.palczynski@nordcloud.com>
2019-09-23 14:07:37 +0200

    use latest master eks module version

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "6.0.0"
+  version = "master"
 
   cluster_name                               = var.cluster_prefix
   subnets                                    = concat(var.private_subnets, var.public_subnets)
