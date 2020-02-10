18a9f3d81decc60f92129d71f5edc4fdfd3d9079
Lukasz Dajek <lukasz.dajek@nordcloud.com>
2020-02-10 17:17:13 +0100

    change in k8s provider

--- main.tf
+++ main.tf
@@ -7,14 +7,12 @@ data "aws_eks_cluster_auth" "cluster" {
 }
 
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
+  host                   = element(concat(data.aws_eks_cluster.cluster[*].endpoint, list("")), 0)
+  cluster_ca_certificate = base64decode(element(concat(data.aws_eks_cluster.cluster[*].certificate_authority.0.data, list("")), 0))
+  token                  = element(concat(data.aws_eks_cluster_auth.cluster[*].token, list("")), 0)
   load_config_file       = false
   version                = "~> 1.10"
 }
-
-
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
